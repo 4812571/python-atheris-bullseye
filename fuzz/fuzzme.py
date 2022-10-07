3f9a4bfb56670df64d0995ea64a3ee53036fc872
Dylan <57853417+4812571@users.noreply.github.com>
2022-10-07 03:40:00 -0400

    Update fuzzme.py

--- fuzz/fuzzme.py
+++ fuzz/fuzzme.py
@@ -2,15 +2,17 @@
 import atheris
 import sys
 
+def MyTestFunction(num):
+    if num == 2:
+        raise Exception("You got it!")
+
+@atheris.instrument_func
 def TestOneInput(data):
     fdp = atheris.FuzzedDataProvider(data)
     num = fdp.ConsumeIntInRange(1, 10)
 
-    if num == 2:
-        raise Exception("You got it!")
+    MyTestFunction(num)
 
 
-#push
-atheris.instrument_all()
 atheris.Setup(sys.argv, TestOneInput)
 atheris.Fuzz()
